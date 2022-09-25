dae6cf47d421a6298e61a16befb16d2d0f7e1a4e
foxstudent102655 <andreypalamarchuk1996@gmail.com>
2022-09-25 17:48:27 +0300

    added localizations to uri

---
+++ lang/ua/mainpage.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'main_page' => 'Головна сторінка',
+    'register' => 'Зарееструватися',
+    'login' => 'Ввійти',
+    'dashboard' => 'Особистий кабінет',
+    'change_language_on_ukrainian' =>  'Змінити мову на українську',
+    'change_language_on_english' => 'Змінити мову на англійську',
+];
--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,31 @@ use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('{locale}')
+    ->where(['locale' => '[a-zA-Z]{2}'])
+    ->middleware('setlocale')
+    ->group(function () {
+
+        Route::get('/', function () {
+            return view('welcome');
+        })->name('home');
+
+        Auth::routes();
+
+        Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+
+        Route::prefix('adminpannel')->middleware(['admin'])->group(function () {
+            Route::get('/', [AdminController::class, 'showPannel'])->middleware(['admin'])->name('adminpannel');
+            Route::resource('users', UserController::class)->except(['show'])->middleware(['admin']);
+            Route::resource('products', ProductController::class)->middleware(['admin']);
+            Route::resource('orders', OrderController::class)->middleware(['admin']);
+        });
+    });
+
 Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+    return redirect(app()->getLocale());
+});
 
-Auth::routes();
-
-Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
-
-Route::prefix('adminpannel')->middleware(['admin'])->group(function () {
-    Route::get('/', [AdminController::class, 'showPannel'])->middleware(['admin'])->name('adminpannel');
-    Route::resource('users', UserController::class)->except(['show'])->middleware(['admin']);
-    Route::resource('products', ProductController::class)->middleware(['admin']);
-    Route::resource('orders', OrderController::class)->middleware(['admin']);
+Route::get('/dashboard', function () {
+    return redirect()->route('dashboard', ['locale' => app()->getLocale()]);
 });
